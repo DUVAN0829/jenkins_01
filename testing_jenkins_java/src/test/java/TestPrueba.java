4a12843c478bbffdeafca68194a804736bc7fc44
Duván González <duglas@MBP-de-Duvan.bbrouter>
2025-12-21 16:03:09 -0500

    Prepara y lanza test unitarios

---
+++ testing_jenkins_java/src/test/java/TestPrueba.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestPrueba {
+
+    @Test
+    void probar() {
+
+        int valor = 23;
+        int valor2 = 12;
+
+        assertEquals(35, valor + valor2);
+
+    }
+
+}
